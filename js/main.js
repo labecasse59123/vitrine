9dc8432d73f63f9902f11f08997ec40189a4f193
apennamen <adrien.pennamen@soprasteria.com>
2020-11-07 18:42:08 +0100

    fix(style): minor fix on css and typos

--- js/main.js
+++ js/main.js
@@ -32,17 +32,6 @@ $(window).on('load', function() {
 		$('.main-warp').removeClass('overflow-hidden');
 	});
 
-	// Search model
-	$('.search-switch').on('click', function() {
-		$('.search-model').fadeIn(400);
-	});
-
-	$('.search-close-switch').on('click', function() {
-		$('.search-model').fadeOut(400,function(){
-			$('#search-input').val('');
-		});
-	});
-
 
 	/*------------------
 		Background Set
